2a40cdd3fe6db6dcb75409085de387e41018218a
kanishkittal17 <mittal@college.harvard.edu>
2020-11-08 23:07:34 -0500

    Initial Import

--- Algorithmic Toolbox/Algorithmic Warm Up/Fibonacci Number Again/fibonacci_number_again.py
+++ Algorithmic Toolbox/Algorithmic Warm Up/Fibonacci Number Again/fibonacci_number_again.py
@@ -13,21 +13,25 @@ def fibonacci_number_again_naive(n, m):
 
     return current
 
+def pisano_period(m):
+    prev, curr = 0, 1
+    for i in range(m * m):
+        prev, curr = curr, (prev + curr) % m
+        if (prev == 0 and curr == 1):
+            return i + 1
+
 
 def fibonacci_number_again(n, m):
     assert 0 <= n <= 10 ** 18 and 2 <= m <= 10 ** 3
-    count = 0
-    fib = [None] * (m + 1)
+    period = pisano_period(m)
+    n = n % period
+    if n <= 1:
+        return n
+    fib = [None] * (n + 1)
     fib[0], fib[1] = 0, 1
-    for i in range(2, m + 1):
-        fib[i] = fib[i - 1] + fib[i - 2]
-        if fib[i] == 0:
-            if (fib[i] + fib[i - 1]) == 1:
-                break
-        count += 1
-    return fib[(n % count)] % m
-
-
+    for i in range(2, n + 1):
+        fib[i] = (fib[i - 1] + fib[i - 2]) % m
+    return fib[n]
 
 if __name__ == '__main__':
     input_n, input_m = map(int, input().split())
