3da3a89c0ded04376bc048140de8331b355bc6b6
kanishkittal17 <mittal@college.harvard.edu>
2020-11-11 23:19:36 -0500

    Last digit of the Sum of Squares of Fibonacci Numbers

--- Algorithmic Toolbox/Algorithmic Warm Up/Last Digit of the Sum of Squares of Fibonacci Numbers/last_digit_of_the_sum_of_squares_of_fibonacci_numbers.py
+++ Algorithmic Toolbox/Algorithmic Warm Up/Last Digit of the Sum of Squares of Fibonacci Numbers/last_digit_of_the_sum_of_squares_of_fibonacci_numbers.py
@@ -15,11 +15,21 @@ def last_digit_of_the_sum_of_squares_of_fibonacci_numbers_naive(n):
 
     return sum([f ** 2 for f in fibonacci_numbers]) % 10
 
+def last_digit_of_fibonacci_number(n):
+    n = n % 60
+    if n <= 1:
+        return n
+    fib = [None] * (n + 1)
+    fib[0], fib[1] = 0, 1
+    for i in range(2, n + 1):
+        fib[i] = (fib[i - 1] + fib[i - 2]) % 10
+    return fib[n]
+
 
 def last_digit_of_the_sum_of_squares_of_fibonacci_numbers(n):
     assert 0 <= n <= 10 ** 18
 
-    type here
+    return (last_digit_of_fibonacci_number(n) * last_digit_of_fibonacci_number(n + 1)) % 10
 
 
 if __name__ == '__main__':
