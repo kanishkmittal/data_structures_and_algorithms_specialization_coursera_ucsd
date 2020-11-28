31df380ad7f4310b300ac5493ffc2d7339b2deaf
kanishkittal17 <mittal@college.harvard.edu>
2020-11-27 20:42:15 -0500

    Divide and Conquer updated

--- Algorithmic Toolbox/Divide-and-Conquer/Majority Element/majority_element.py
+++ Algorithmic Toolbox/Divide-and-Conquer/Majority Element/majority_element.py
@@ -1,6 +1,6 @@
 # python3
 
-
+import collections
 def majority_element_naive(elements):
     assert len(elements) <= 10 ** 5
     for e in elements:
@@ -11,8 +11,12 @@ def majority_element_naive(elements):
 
 
 def majority_element(elements):
-    assert len(elements) <= 10 ** 5
-    type here
+#        assert len(elements) <= 10 ** 5
+    count = collections.Counter(elements)
+    max_el = max(count.keys(), key=count.get)
+    if count[max_el] > len(elements)/2:
+        return 1
+    return 0
 
 
 if __name__ == '__main__':
